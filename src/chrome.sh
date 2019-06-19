1c463fcf0fc12c3c5b4ab422a58525ddcbace138
Iman Akbari <imakbari@gmail.com>
2019-06-18 21:30:14 -0400

    Delete obsolete file

--- src/chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SSLFILE=/usr/src/app/out/keys.ssl
-
-sudo touch $SSLFILE
-sudo chown chrome $SSLFILE
-
-chromium-browser --no-sandbox --headless --disable-gpu --ssl-key-log-file="$SSLFILE"
-
